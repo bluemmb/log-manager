8af7a6ff94877339936a269fdffe25135afcadbe
Mohammad Eftekhari <bluemmb22@gmail.com>
2021-09-27 15:23:15 +0330

    Make Dotenv provider singleton

--- core/src/main/java/org/me/core/Providers/DotEnvProvider.java
+++ core/src/main/java/org/me/core/Providers/DotEnvProvider.java
@@ -2,10 +2,12 @@ package org.me.core.Providers;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
+import com.google.inject.Singleton;
 import io.github.cdimascio.dotenv.Dotenv;
 
 public class DotEnvProvider extends AbstractModule {
     @Provides
+    @Singleton
     Dotenv provideDotenv(Dotenv implementation) {
         return Dotenv.load();
     }
