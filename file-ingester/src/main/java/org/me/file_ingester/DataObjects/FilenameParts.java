fe10ccfa7f2929dd1b9b756dc927f71f5f35e9da
Mohammad Eftekhari <bluemmb22@gmail.com>
2021-09-27 15:14:33 +0330

    Add validation for component names

--- file-ingester/src/main/java/org/me/file_ingester/DataObjects/FilenameParts.java
+++ file-ingester/src/main/java/org/me/file_ingester/DataObjects/FilenameParts.java
@@ -1,11 +1,22 @@
 package org.me.file_ingester.DataObjects;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import org.me.core.Container;
+
 import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class FilenameParts {
     private static final String regex = "^(\\w+)\\-(\\d{4}_\\d{2}_\\d{2}\\-\\d{2}_\\d{2}_\\d{2})\\.log$";
+    private static final List<String> validComponentNames;
+
+    static {
+        Dotenv dotenv = Container.get(Dotenv.class);
+        validComponentNames = Arrays.asList(dotenv.get("COMPONENTS").split(","));
+    }
 
     public String componentName;
     public String datetimeString;
@@ -25,6 +36,12 @@ public class FilenameParts {
         if ( matcher.groupCount() != 2 )
             return null;
 
-        return new FilenameParts(matcher.group(1), matcher.group(2));
+        String componentName = matcher.group(1);
+        String datetimeString = matcher.group(2);
+
+        if ( ! validComponentNames.contains(componentName) )
+            return null;
+
+        return new FilenameParts(componentName, datetimeString);
     }
 }
