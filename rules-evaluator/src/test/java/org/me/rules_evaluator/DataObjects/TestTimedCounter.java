3da9705044fc44fd10ab1efbb4928e539da35390
Mohammad Eftekhari <bluemmb22@gmail.com>
2021-10-01 11:53:17 +0330

    Add test for casse TimedCounter size overflows

--- rules-evaluator/src/test/java/org/me/rules_evaluator/DataObjects/TestTimedCounter.java
+++ rules-evaluator/src/test/java/org/me/rules_evaluator/DataObjects/TestTimedCounter.java
@@ -11,7 +11,7 @@ public class TestTimedCounter {
     public void test() {
         TimedCounter timedCounter = new TimedCounter(4);
         Date now = new Date();
-        Date nowMinusOne = new Date(now.getTime() - (    60L * 1000L));
+        Date nowMinusOne = new Date(now.getTime() - (60L * 1000L));
         Date nowMinusThree = new Date(now.getTime() - (3 * 60L * 1000L));
 
         timedCounter.add(now);
@@ -26,5 +26,26 @@ public class TestTimedCounter {
 
         Integer[] report2 = timedCounter.reportCounts(5, nowMinusOne);
         assertArrayEquals(new Integer[] {1, 0, 3, 0, 0}, report2);
+    }
+
+    @Test
+    public void testSizeOverflow() {
+        TimedCounter timedCounter = new TimedCounter(3);
+        Date now = new Date();
+        Date nowMinusOne = new Date(now.getTime() - (60L * 1000L));
+        Date nowMinusTwo = new Date(now.getTime() - (2 *  60L * 1000L));
+        Date nowMinusThree = new Date(now.getTime() - (3 * 60L * 1000L));
+
+        timedCounter.add(nowMinusThree);
+        timedCounter.add(nowMinusTwo);
+        timedCounter.add(nowMinusOne);
+
+        Integer[] report = timedCounter.reportCounts(5, now);
+        assertArrayEquals(new Integer[] {0, 1, 1, 1, 0}, report);
+
+        timedCounter.add(now);
+
+        Integer[] report2 = timedCounter.reportCounts(5, now);
+        assertArrayEquals(new Integer[] {1, 1, 1, 0, 0}, report2);
     }
 }
