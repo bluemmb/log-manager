d622dd9e8a0d7a8b0754608ba9b0dc57b43f0c06
Mohammad Eftekhari <bluemmb22@gmail.com>
2021-10-02 10:44:42 +0330

    Update logs clean script

--- assets/logs/clean.sh
+++ assets/logs/clean.sh
@@ -1,3 +1,7 @@
-#/bin/sh
-git clean -fX ./temp/
-git clean -fX ./files/
+#!/bin/bash
+
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+echo $SCRIPT_DIR;
+
+git clean -fX $SCRIPT_DIR/temp/
+git clean -fX $SCRIPT_DIR/files/
